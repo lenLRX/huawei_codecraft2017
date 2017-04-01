bd7494112fcf7f9d2e8b59bcae62f30bfc423e80
abceqqqqq <abceqqqqq@126.com>
2017-04-01 23:30:59 +0800

    git save before checkout

--- cdn/Firefly.cpp
+++ cdn/Firefly.cpp
@@ -61,7 +61,7 @@ bool FireflySolver::CostOfFly(Firefly& fly,int i){
 	if(b){
 		for(int i = 0;i < MaxEdgeNum;i++){
 			int e = lr.G.array_Vertex_EdgesOut[-1 * MaxEdgeNum + i];
-			if(lr.G.array_Edge_x[e])
+			if(lr.G.array_Edge_x[e] > 0)
 			    fly.bits[lr.G.array_Edge_to[e]] = true;
 			else
 			    fly.bits[lr.G.array_Edge_to[e]] = false;
